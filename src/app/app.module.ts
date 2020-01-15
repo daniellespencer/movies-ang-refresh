69d22b75ce32ed7316169158c5ce2fdd6b8082be
Danielle Spencer <d.m.spencer.0486@gmail.com>
2020-01-15 17:58:01 -0600

    push changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MoviesListComponent } from './movies-list/movies-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { MoviesListComponent } from './movies-list/movies-list.component';
     MoviesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
