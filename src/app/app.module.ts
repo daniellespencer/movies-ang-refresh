46341c1ee2dc47a47904036049640fe87aa3df23
Danielle Spencer <d.m.spencer.0486@gmail.com>
2020-01-15 18:57:45 -0600

    updated button styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MoviesListComponent } from './movies-list/movies-list.component';
 import { FormsModule } from '@angular/forms';
-import { BooksListComponent } from './books-list/books-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesListComponent,
-    BooksListComponent
   ],
   imports: [
     BrowserModule,
